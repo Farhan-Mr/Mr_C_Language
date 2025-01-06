0c9b69a1dcecc0d229a8260d4c57fa90084a18f2
Farhan87 <farhat.hp2022@gmail.com>
2025-01-06 18:22:35 +0530

    Sum of given Array

---
+++ Array/sum_of_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main (){
+
+    int a;
+    printf("\nEnter size of array: ");
+    scanf("%d", &a);
+    int arr[a];
+    
+    printf("Enter the elements of array - ");
+    for( int i = 0; i<a; i++ )
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("\nGiven elements inside the array is - ");
+    for(int i=0; i<a; i++)
+    {
+        printf("%d  ",arr[i]);
+    }
+    int sum = 0;
+    for(int i=0; i<a; i++){
+        sum += arr[i];
+    }
+    printf("\nSum of given array is - %d", sum);
+    printf("\nThanku");
+
+    return 0;
+}
