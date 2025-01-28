d4cb47c3a6fbbbcd9e0f5bb3578eced66d57e595
Farhan87 <farhat.hp2022@gmail.com>
2025-01-28 16:51:28 +0530

    use Switch Case to display Days name

---
+++ Playing with Number/Loop_1_to_10.c
@@ -0,0 +1,10 @@
+/*  Write a C program to display the first 10 natural numbers using a `for` loop. 
+    Debug and execute the program*/
+
+    #include<stdio.h>
+    int main (){
+
+        
+
+        return 0;
+    }
