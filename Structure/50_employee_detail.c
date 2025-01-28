472c867365501229026b7061976bd9feb04202ce
Farhan87 <farhat.hp2022@gmail.com>
2025-01-28 13:03:19 +0530

    Book Detail in file

--- Structure/50_employee_detail.c
+++ Structure/50_employee_detail.c
@@ -12,14 +12,14 @@
 
     int main (){
 
-        struct Employee emp[2];
+        struct Employee emp[50];
         printf("Enter 50 Employee Detail \n1. Empid \n2. Name \n3. Salary \n4. Age\n");
 
-        for(int i=0; i<2; i++){
+        for(int i=0; i<50; i++){
             scanf("%d%s%d%d",&emp[i].Empid,&emp[i].name,&emp[i].salary,&emp[i].age);
         }
         printf("\n\n");
-        for(int i=0; i<2; i++)
+        for(int i=0; i<50; i++)
         {
             printf("\nDetail of %d. Employee - \nEmpid = %d\nName = %s\nSalary = %d\nAge = %d",(i+1),emp[i].Empid,emp[i].name,emp[i].salary,emp[i].age);
         }
