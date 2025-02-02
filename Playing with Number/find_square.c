7c94ce181e6ff64ec7231a9921129cf4455e7d62
Farhan87 <farhat.hp2022@gmail.com>
2025-02-02 21:54:47 +0530

    Find Square of Given Number

---
+++ Playing with Number/find_square.c
@@ -0,0 +1,13 @@
+/* WAP to find Square of number */
+
+#include<stdio.h>
+int main (){
+
+    int a;
+    printf("Enter number : ");
+    scanf("%d",&a);
+
+    printf("Square of %d is %d",a,(a*a));
+
+    return 0;
+}
