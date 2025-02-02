f06f710179cb287aa16b5219ed668fe08700d39d
Farhan87 <farhat.hp2022@gmail.com>
2025-02-02 21:58:53 +0530

    Find Cube

---
+++ Playing with Number/find_cube.c
@@ -0,0 +1,13 @@
+/* WAP to find Cube of number */
+
+#include<stdio.h>
+int main (){
+
+    int a;
+    printf("Enter number : ");
+    scanf("%d",&a);
+
+    printf("Square of %d is %d",a,(a*a*a));
+
+    return 0;
+}
