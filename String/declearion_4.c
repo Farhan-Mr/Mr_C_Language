c58728a8cd3d3a47b55bbc65577f9b997378da6a
Farhan87 <farhat.hp2022@gmail.com>
2025-02-03 11:52:17 +0530

    Decleartion of string method 4

---
+++ String/declearion_4.c
@@ -0,0 +1,11 @@
+/* WAP to declear string with the help of pointer - */
+
+#include<stdio.h>
+int main (){
+
+    char *str = "Today is Friday";
+        
+            printf("%s",str);
+
+    return 0;
+}
