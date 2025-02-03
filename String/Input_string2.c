9a2365ab84fa74cec1299a1e65df0717140a3300
Farhan87 <farhat.hp2022@gmail.com>
2025-02-03 12:02:39 +0530

    Input string Method 2

---
+++ String/Input_string2.c
@@ -0,0 +1,14 @@
+/* WAP to declear string and input from user - */
+
+//  Method 2
+
+#include<stdio.h>
+int main (){
+
+    char str[50] ; 
+    printf("Enter string ->  ");
+        scanf("%s",str);
+        printf("%s",str);
+
+    return 0;
+}
