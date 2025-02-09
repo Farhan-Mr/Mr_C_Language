8f028609c1f8b4c922a2090d4c5189a48ae2c210
Farhan87 <farhat.hp2022@gmail.com>
2025-02-09 23:52:12 +0530

    Factor using recursive function

---
+++ Recursion/factor.c
@@ -0,0 +1,30 @@
+/* WAP to enter a number and find its factor using recursive function */
+
+#include<stdio.h>
+
+void factor(int x,int i);
+
+int main(){
+
+    int a;
+    int i = 1;
+    printf("Enter number : ");
+    scanf("%d",&a);
+
+    // Function calling 
+      factor(a,i);
+
+    return 0;
+}
+
+void factor(int a,int i){
+   
+    if(i>a){
+        return;
+    }
+    if(a % i == 0){
+        printf("%d ",i);
+    }
+    factor(a,i+1);
+    
+}
