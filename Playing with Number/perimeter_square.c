9c75f8c9f9fd46900c63a95ca6ae8fa537472610
Farhan87 <farhat.hp2022@gmail.com>
2025-02-13 20:05:57 +0530

    Perimeter of Square

---
+++ Playing with Number/perimeter_square.c
@@ -0,0 +1,15 @@
+/* WAP to find Perimeter of square */
+
+#include<stdio.h>
+int main (){
+
+    printf("\n");
+    int s;
+    printf("Enter Perimeter of Square - ");
+    scanf("%d",&s);
+    
+    // Perimeter of square - 4*s 
+
+    printf("Area of square : %d",4*s);
+    return 0;
+}
