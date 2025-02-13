29defe88f8e38c72d7ef09f5d6cd1e28566f8aa9
Farhan87 <farhat.hp2022@gmail.com>
2025-02-13 20:09:33 +0530

    Area of Rectangle

---
+++ Playing with Number/Area_of_rectangle.c
@@ -0,0 +1,17 @@
+/* WAP to find Area of Rectangle */
+
+#include<stdio.h>
+int main (){
+
+    int l,w;
+    printf("Enter length of rectangle - ");
+    scanf("%d",&l);
+
+    printf("Enter width of rectangle - ");
+    scanf("%d",&w);
+    
+    // Area of Rectangle l*w 
+
+    printf("Area of Rectangle : %d",l*w);
+    return 0;
+}
