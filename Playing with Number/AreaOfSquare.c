67f271226cb17d69207861bccb413ed93c3960e7
Farhan87 <farhat.hp2022@gmail.com>
2025-02-13 20:02:48 +0530

    Area of square

---
+++ Playing with Number/AreaOfSquare.c
@@ -0,0 +1,14 @@
+/* WAP to find area of square */
+
+#include<stdio.h>
+int main (){
+
+    int s;
+    printf("Enter sides of Square - ");
+    scanf("%d",&s);
+    
+    // Area of square - s*s 
+
+    printf("Area of square : %d",s*s);
+    return 0;
+}
