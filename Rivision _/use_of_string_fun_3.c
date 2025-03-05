684e099b113f199ff860ae7b458095bc29ded750
Farhan87 <farhat.hp2022@gmail.com>
2025-03-05 09:39:17 +0530

    use of strcat function

--- Rivision _/use_of_string_fun_3.c
+++ Rivision _/use_of_string_fun_3.c
@@ -1,1 +1,26 @@
 /* WAP to use of strcat function */
+
+
+#include<stdio.h>
+#include<string.h>
+    int main (){
+
+        char str[10];
+        char strr[10];
+
+        printf("Enter 1st string : ");
+        scanf("%s",&str);
+
+        printf("Enter 2nd String : ");
+        scanf("%s",&strr);
+
+        printf("\n1 st string is : %s",str);
+        printf("\n2 nd string is : %s",strr);
+
+        // strcat
+
+        printf("\n1st to 2nd -> %s ",strcat(str,strr));
+
+
+        return 0;
+    }
