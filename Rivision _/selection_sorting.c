d0a7af619aea2fc5183b046ab9fac188f0b6699f
Farhan87 <farhat.hp2022@gmail.com>
2025-03-08 19:38:19 +0530

    Short Array using Selection Sorting

---
+++ Rivision _/selection_sorting.c
@@ -0,0 +1,40 @@
+/* WAP to sort the array in ascending order using selection sorting  */
+
+#include<stdio.h>
+int main()
+{
+
+ int arr[7] = { 73,20,4,45,14,17,102};
+ int a = 7;
+
+ printf("\nBefore sorting : ");
+ for(int i=0; i<a; i++)
+ {
+     printf("%d ",arr[i]);
+ }
+
+ for(int i=0; i<a-1; i++)
+ {
+    int min = i ;
+    for(int j=i+1; j<a; j++)
+    {
+        if(arr[min]>arr[j])
+        {
+            min = j ;
+        }
+    }
+    int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+ }
+ printf("\n\nAfter sorting - \n");
+ printf("Ascending order : ");
+
+ for(int i=0; i<a; i++)
+ {
+    printf("%d ",arr[i]);
+ }
+
+
+    return 0;
+}
