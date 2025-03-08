1aa5ed69921bfd3becd0547fa6fa38c798057f99
Farhan87 <farhat.hp2022@gmail.com>
2025-03-08 19:39:41 +0530

    Swaping two number (With retrun value)

---
+++ Rivision _/swap_2_number.c
@@ -0,0 +1,34 @@
+/* WAP to swap Two number ( Call by reference )*/
+
+#include<stdio.h>
+
+    void swap(int *a, int *b);
+
+    int main ()
+    {
+        int a,b;
+        printf("Enter a : ");
+        scanf("%d",&a);
+
+        printf("Enter b : ");
+        scanf("%d",&b);
+
+        printf("Before swaping :\n");
+        printf("a : %d \nb : %d",a,b);
+
+        // function call -
+
+        swap(&a,&b);
+
+        return 0;
+    }
+  void swap(int *a, int *b)
+  {
+    int temp = *a ;
+        *a   = *b ;
+        *b   = temp ;
+
+        printf("\nAfter swaping :\n");
+        printf("a : %d \nb : %d",*a,*b);
+  
+  }  
